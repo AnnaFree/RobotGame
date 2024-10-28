8e1bae7360a0617ee9e5ab3cfff2c10bad049e35
AnnaFree <annagrace660@gmail.com>
2024-10-28 15:21:49 -0400

    Main push

---
+++ Source/RobotGame/Public/RobotEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RobotCharacterBase.h"
+#include "RobotEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTGAME_API ARobotEnemy : public ARobotCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/RobotGame/Public/RobotMainCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RobotCharacterBase.h"
+#include "RobotMainCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTGAME_API ARobotMainCharacter : public ARobotCharacterBase
+{
+	GENERATED_BODY()
+	
+};
